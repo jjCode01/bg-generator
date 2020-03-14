55a9a846019c65d43ad92bdbf39d067976cd44ec
Jesse <code@seqmanagement.com>
2020-03-14 14:42:48 -0700

    Refactored code

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,10 @@
 let css = document.querySelector("h3");
-let color1 = document.querySelector(".color1");
-let color2 = document.querySelector(".color2");
+let color = document.querySelectorAll(".color");
 let body = document.getElementById ("gradient");
 
 let setGradient = () =>{
-    body.style.background = 
-        "linear-gradient(to right, " 
-        + color1.value 
-        + ", " 
-        + color2.value + ")";
-
-        css.textContent = body.style.background + ";";
+    body.style.background = `linear-gradient(to right, ${color[0].value}, ${color[1].value})`;
+    css.textContent = `${body.style.background};`;
 }
 
-color1.addEventListener("input", setGradient)
-
-color2.addEventListener("input", setGradient)
+[...color].forEach(clr => clr.addEventListener("input", setGradient))
